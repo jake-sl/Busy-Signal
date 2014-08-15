909da965d9927907834adc507c01bc8ba2ea93a9
arcym <andrewmcp333@gmail.com>
2014-08-15 17:00:43 -0400

    Defined a function for toggling the light.

--- busysignal.js
+++ busysignal.js
@@ -17,3 +17,28 @@ function isAvailable() {
 
 
 }
+
+var state = true;
+
+function toggleLight()
+{
+	console.log("!!");
+	
+	$("button").click(function()
+	{
+		state = !state;
+		
+		jQuery.ajax({
+			type: "PUT",
+			url: "http://10.0.0.180/api/newdeveloper/lights/3/state",
+			data: JSON.stringify({
+				on: state
+			}),
+			processData: false
+		})
+		.then(function(data)
+		{
+			console.log(JSON.stringify(data, undefined, 2));
+		});
+	});
+}
