8edc7de16f8b6650ec7bfa189f386c7447277cd0
jake-sl <jake_b@me.com>
2014-08-15 16:23:09 -0400

    New functions

--- busysignal.js
+++ busysignal.js
@@ -1,4 +1,19 @@
 // Copyright (c) 2014 Andrew Rodgers, Andrew McPherson, and Jake Brown. All rights reserved.
 // MIT license
 
+function isBusy() {
 
+   
+}
+
+
+function isAway() {
+
+
+}
+
+
+function isAvailable() {
+
+
+}
